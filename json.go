8a0b23af1634ae1be127fae2f1be1d61673d6de6
“H-H1 <2353367985@qq.com>
2022-09-25 14:18:49 +0800

    Create json.go

---
+++ json.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Metric struct {
+	Name  string `json:"name"`
+	Value int64  `json:"value"`
+}
+
+
+// demo_select6.go
+func main() {
+	ch := make(chan int, 1)
+
+	// 发送1个数据关闭channel
+	ch <- 1
+	//close(ch)
+	// 不停读数据直到channel没有有效数据
+	for {
+		select {
+		case v, ok := <-ch:
+			fmt.Println("v:", v, ", ok:", ok )
+			if !ok {
+				fmt.Println(v,ok)
+			}
+			if v==2 {
+	     		break
+			}
+		default:
+			ch <-2
+		}
+
+
+	}
+}
+
